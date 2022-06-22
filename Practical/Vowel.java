a4fdc0cda7c8e784476824bff5b10acb85dc4303
sufiyashahin <107432417+sufiyashahin@users.noreply.github.com>
2022-06-22 12:35:55 +0530

    Add files via upload

---
+++ Practical/Vowel.java
@@ -0,0 +1,28 @@
+package Java;
+
+public class Vowel {
+
+	public static void main(String[] args) {
+		 char ch = 'I';
+
+	        switch (ch) {
+	            case 'a':
+	            case 'e':
+	            case 'i':
+	            case 'o':
+	            case 'u':
+	            case 'A':
+	            case 'E':
+	            case 'I':
+	            case 'O':
+	            case 'U':
+	            	
+	                System.out.println(ch + " is vowel");
+	                break;
+	            default:
+	                System.out.println(ch + " is consonant");
+	        }
+
+	}
+
+}
