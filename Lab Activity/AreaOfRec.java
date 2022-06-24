2b6980bc12aecd58f452b5118558ccc23d43d30d
sufiyashahin <107432417+sufiyashahin@users.noreply.github.com>
2022-06-24 11:28:14 +0530

    Add files via upload

---
+++ Lab Activity/AreaOfRec.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class AreaOfRec {
+
+	public static void main(String[] args) {
+		area();
+  //calling method
+	}
+
+	private static void area() {
+		
+		//variable declaration
+		int length=10;
+		int width=5;
+		
+		//logic for printing area of rectangle
+		int area=length*width;
+		
+		//print for area of rectangle
+		System.out.println(area);
+		
+	}
+
+}
